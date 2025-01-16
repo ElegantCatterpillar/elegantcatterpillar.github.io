aea5b7bb0ae0ccc73bf474bc6da76454120adb6b
ElegantCaterpillar <oscartlgarciatl@gmail.com>
2025-01-15 21:44:14 -0700

    cambios menores

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="flex flex-col h-screen">
+      <LayoutNavbar />
+      <div class="flex-1">
+        <slot />
+      </div>
+      <LayoutFooter />
+    </div>
+  </template>
+  
